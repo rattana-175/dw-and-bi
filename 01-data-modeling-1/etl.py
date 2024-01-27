35bb33f4543250006c653d068e51a18f0855eee1
rattana-175 <157203324+rattana-175@users.noreply.github.com>
2024-01-27 11:19:55 +0000

    Update data

--- 01-data-modeling-1/etl.py
+++ 01-data-modeling-1/etl.py
@@ -6,22 +6,6 @@ from typing import List
 import psycopg2
 
 
-def get_files(filepath: str) -> List[str]:
-    """
-    Description: This function is responsible for listing the files in a directory
-    """
-
-    all_files = []
-    for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, "*.json"))
-        for f in files:
-            all_files.append(os.path.abspath(f))
-
-    num_files = len(all_files)
-    print(f"{num_files} files found in {filepath}")
-
-    return all_files
-
 
 def process(cur, conn, filepath):
     # Get list of files from filepath
