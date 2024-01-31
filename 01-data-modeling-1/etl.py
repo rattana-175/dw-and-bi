f2baaa42f39cd47b22bb425cb3d203b487ee21f0
rattana-175 <157203324+rattana-175@users.noreply.github.com>
2024-01-31 02:52:18 +0000

    add function get_files

--- 01-data-modeling-1/etl.py
+++ 01-data-modeling-1/etl.py
@@ -5,6 +5,21 @@ from typing import List
 
 import psycopg2
 
+def get_files(filepath: str) -> List[str]:
+    """
+    Description: This function is responsible for listing the files in a directory
+    """
+
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    num_files = len(all_files)
+    print(f"{num_files} files found in {filepath}")
+
+    return all_files
 
 
 def process(cur, conn, filepath):
